a5736349706a445ed39d151b7b3b5f77699516ab
ankita109 <dubeyankita107@gmail.com>
2023-07-28 00:04:57 +0530

    latest update

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app =express();
 const dbconnection=require("./db")
 const usersRoute=require("./routes/usersRoute")
 const postsRoute=require("./routes/postsRoute")
-const path = require("path");
+
 
 app.use(express.json({limit:'25mb'}))
 
@@ -13,11 +13,4 @@ app.use('/api/posts/', postsRoute)
 
 const port = process.env.PORT || 5000;
 
-
-if (process.env.NODE_ENV === "production") {
-app.use('/',express.static( "/client/build"));
-app.get("*", (req, res) => {
-res.sendFile(path.resolve(__dirname, "client/build/index.html"));
-});
-}
 app.listen(port,()=> console.log(`Server running on port ${port}`));
